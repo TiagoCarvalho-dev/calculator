b54922fa3a074f945ab97ac9366f6421a0749a67
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-05-24 13:24:45 -0300

    Grouped all button listeners into one
    Now the operations function needs to be updated to the eventListeners changes

--- script.js
+++ script.js
@@ -4,29 +4,21 @@ let operator;
 let displayValue = '';
 let result = 0;
 
-document.getElementById('zeroButton').addEventListener('click', () => changeDisplayValue(0));
-document.getElementById('oneButton').addEventListener('click', () => changeDisplayValue(1));
-document.getElementById('twoButton').addEventListener('click', () => changeDisplayValue(2));
-document.getElementById('threeButton').addEventListener('click', () => changeDisplayValue(3));
-document.getElementById('fourButton').addEventListener('click', () => changeDisplayValue(4));
-document.getElementById('fiveButton').addEventListener('click', () => changeDisplayValue(5));
-document.getElementById('sixButton').addEventListener('click', () => changeDisplayValue(6));
-document.getElementById('sevenButton').addEventListener('click', () => changeDisplayValue(7));
-document.getElementById('eightButton').addEventListener('click', () => changeDisplayValue(8));
-document.getElementById('nineButton').addEventListener('click', () => changeDisplayValue(9));
-document.getElementById('dotButton').addEventListener('click', () => changeDisplayValue('.'));
+// FIXES:
+// FIX %, /, X, = AND . BUTTONS
 
-document.getElementById('addButton').addEventListener('click', () => handleOperatorsEvents('+'));
-document.getElementById('subtractButton').addEventListener('click', () => handleOperatorsEvents('-'));
-document.getElementById('multiplyButton').addEventListener('click', () => handleOperatorsEvents('*'));
-document.getElementById('divideButton').addEventListener('click', () => handleOperatorsEvents('/'));
-document.getElementById('percentageButton').addEventListener('click', () => handleOperatorsEvents('%'));
-document.getElementById('equalsButton').addEventListener('click', () => calculateResult(operator));
+document.querySelectorAll('#calculatorButtons button').forEach((node) => {
+  node.addEventListener('click', () => handleButtonClicks(node.textContent));
+});
 
-document.getElementById('allClearButton').addEventListener('click', () => clearData('allClear'));
-document.getElementById('clearButton').addEventListener('click', () => clearData('clear'));
+function changeDisplayValue(value) {
+  displayValue += value;
+  document.getElementById('displayScreen').textContent = displayValue;
+}
 
-function handleOperatorsEvents(newOperator) {
+function handleButtonClicks(newOperator) {
+  
+  
   if(number1 === undefined) {
     number1 = +displayValue;
     operator = newOperator;
@@ -36,36 +28,32 @@ function handleOperatorsEvents(newOperator) {
   }
 }
 
-function changeDisplayValue(value) {
-  displayValue += value;
-  document.getElementById('displayScreen').textContent = displayValue;
-}
-
 function calculateResult(operator) {
   number2 = +displayValue;
   operate(operator);
-  showResult();
+  showResult(result);
 }
 
-function showResult() {
+function showResult(result) {
   displayValue = '';
   changeDisplayValue(result);
   displayValue = '';
   number1 = result;
+  number2 = undefined;
   operator = undefined;
 }
 
 function clearData(info) {
   if (info === 'allClear') {
     displayValue = '';
-    changeDisplayValue('');
+    document.getElementById('displayScreen').textContent = 0;
     number1 = undefined;
     number2 = undefined;
     operator = undefined;
     result = 0;
   } else {
     displayValue = '';
-    changeDisplayValue('');
+    document.getElementById('displayScreen').textContent = 0;
   }
 }
 
@@ -85,6 +73,9 @@ function operate(operator) {
       break;
     case '%':
       percentage(number1, number2);
+      break;
+    case '=':
+      calculateResult(operator);
       break;
   }
 }
