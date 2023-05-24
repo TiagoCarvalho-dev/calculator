696d6830a2c2f900bc5dd766af4466f79cac13c6
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-05-24 13:55:33 -0300

    Changed the handleButtonClicks() to use switch
    Also made small changes to the clear and clearAll functions.

--- script.js
+++ script.js
@@ -7,25 +7,41 @@ let result = 0;
 // FIXES:
 // FIX %, /, X, = AND . BUTTONS
 
-document.querySelectorAll('#calculatorButtons button').forEach((node) => {
+document.querySelectorAll('#calculatorButtons button').forEach(node => {
   node.addEventListener('click', () => handleButtonClicks(node.textContent));
 });
+
+function handleButtonClicks(buttonValue) {
+  switch (true) {
+    case (Number.isInteger(+buttonValue) || buttonValue === '.'):
+      changeDisplayValue(buttonValue);
+      break;
+    case (buttonValue === 'AC' || buttonValue === 'C'):
+      buttonValue === 'AC' ? clearAllData() : clearDisplayStatus();
+      break;
+    case (buttonValue === '='):
+      calculateResult(operator);
+      break;
+    default:
+      if(number1 === undefined) {
+        number1 = +displayValue;
+        operator = buttonValue;
+        displayValue = '';
+      } else if (!number1 === undefined && !operator === undefined) {
+        number2 = number1;
+        console.log(number1, number2, operator, result);
+        calculateResult(buttonValue);
+      } else {
+        console.log(number1, number2, operator, result);
+        calculateResult(buttonValue);
+      }
+      break;
+  }
+}
 
 function changeDisplayValue(value) {
   displayValue += value;
   document.getElementById('displayScreen').textContent = displayValue;
-}
-
-function handleButtonClicks(newOperator) {
-  
-  
-  if(number1 === undefined) {
-    number1 = +displayValue;
-    operator = newOperator;
-    displayValue = '';
-  } else {
-    calculateResult(newOperator);
-  }
 }
 
 function calculateResult(operator) {
@@ -43,18 +59,17 @@ function showResult(result) {
   operator = undefined;
 }
 
-function clearData(info) {
-  if (info === 'allClear') {
-    displayValue = '';
-    document.getElementById('displayScreen').textContent = 0;
+function clearAllData() {
+    clearDisplayStatus();
     number1 = undefined;
     number2 = undefined;
     operator = undefined;
     result = 0;
-  } else {
-    displayValue = '';
-    document.getElementById('displayScreen').textContent = 0;
-  }
+}
+
+function clearDisplayStatus() {
+  displayValue = '';
+  document.getElementById('displayScreen').textContent = 0;
 }
 
 function operate(operator) {
