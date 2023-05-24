ec05afe60a888c14cbce908597ae6f15e18ab8b2
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-05-23 23:51:30 -0300

    Added functionality to all the buttons
    Also created function for the AC and C buttons, to clear all data from the calculator and clear the display respectively.

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ let operator;
 let displayValue = '';
 let result = 0;
 
+document.getElementById('zeroButton').addEventListener('click', () => changeDisplayValue(0));
 document.getElementById('oneButton').addEventListener('click', () => changeDisplayValue(1));
 document.getElementById('twoButton').addEventListener('click', () => changeDisplayValue(2));
+document.getElementById('threeButton').addEventListener('click', () => changeDisplayValue(3));
+document.getElementById('fourButton').addEventListener('click', () => changeDisplayValue(4));
+document.getElementById('fiveButton').addEventListener('click', () => changeDisplayValue(5));
+document.getElementById('sixButton').addEventListener('click', () => changeDisplayValue(6));
+document.getElementById('sevenButton').addEventListener('click', () => changeDisplayValue(7));
+document.getElementById('eightButton').addEventListener('click', () => changeDisplayValue(8));
+document.getElementById('nineButton').addEventListener('click', () => changeDisplayValue(9));
+document.getElementById('dotButton').addEventListener('click', () => changeDisplayValue('.'));
+
 document.getElementById('addButton').addEventListener('click', () => handleOperatorsEvents('+'));
-document.getElementById('equalsButton').addEventListener('click', () => displayResult('='));
+document.getElementById('subtractButton').addEventListener('click', () => handleOperatorsEvents('-'));
+document.getElementById('multiplyButton').addEventListener('click', () => handleOperatorsEvents('*'));
+document.getElementById('divideButton').addEventListener('click', () => handleOperatorsEvents('/'));
+document.getElementById('percentageButton').addEventListener('click', () => handleOperatorsEvents('%'));
+document.getElementById('equalsButton').addEventListener('click', () => calculateResult(operator));
+
+document.getElementById('allClearButton').addEventListener('click', () => clearData('allClear'));
+document.getElementById('clearButton').addEventListener('click', () => clearData('clear'));
 
 function handleOperatorsEvents(newOperator) {
   if(number1 === undefined) {
@@ -15,10 +32,7 @@ function handleOperatorsEvents(newOperator) {
     operator = newOperator;
     displayValue = '';
   } else {
-    number2 = +displayValue;
-    operate(newOperator);
-    displayValue = '';
-    showResult();
+    calculateResult(newOperator);
   }
 }
 
@@ -27,9 +41,32 @@ function changeDisplayValue(value) {
   document.getElementById('displayScreen').textContent = displayValue;
 }
 
+function calculateResult(operator) {
+  number2 = +displayValue;
+  operate(operator);
+  showResult();
+}
+
 function showResult() {
+  displayValue = '';
   changeDisplayValue(result);
   displayValue = '';
+  number1 = result;
+  operator = undefined;
+}
+
+function clearData(info) {
+  if (info === 'allClear') {
+    displayValue = '';
+    changeDisplayValue('');
+    number1 = undefined;
+    number2 = undefined;
+    operator = undefined;
+    result = 0;
+  } else {
+    displayValue = '';
+    changeDisplayValue('');
+  }
 }
 
 function operate(operator) {
@@ -57,17 +94,17 @@ function addition(a, b) {
 }
 
 function subtraction(a, b) {
-  return a - b;
+  return result = a - b;
 }
 
 function multiplication(a, b) {
-  return a * b;
+  return result = a * b;
 }
 
 function division(a, b) {
-  return a / b;
+  return result = a / b;
 }
 
 function percentage(a, b) {
-  return a + (a * b) / 100;
+  return result = a + (a * b) / 100;
 }
